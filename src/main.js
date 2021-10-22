33ee8ec818efe49cc1acc3cfd768438088c77f6f
Kai Kuller <kavakai@gmail.com>
2021-10-22 17:52:45 -0600

    Add eventListener functions for buttons

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // Create variables targetting the relevant DOM elements here 👇
 var coverImage = document.querySelector('.cover-image');
-// coverImage.src = './assets/ruffles.jpg'
 var coverTitle = document.querySelector('.cover-title');
 var tagline1 = document.querySelector('.tagline-1');
 var tagline2 = document.querySelector('.tagline-2');
+
 var homeButton = document.querySelector('.home-button hidden');
 var randomButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var makeNewButton = document.querySelector('.make-new-button');
+
+var saveCoverPage = document.querySelector('.saved-view');
+var homePage = document.querySelector('.home-view');
+var makeYourOwn = document.querySelectorAll('.form-view');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -17,7 +21,8 @@ var currentCover = new Cover(coverImage, coverImage, tagline1, tagline2);
 
 // Add your event listeners here 👇
 randomButton.addEventListener('click', changeCover);
-
+saveCoverButton.addEventListener('click', changeSavePage);
+makeNewButton.addEventListener('click', changeToMakeYourOwn)
 // .unload method
 //class at make a new book form
 
@@ -27,10 +32,24 @@ randomButton.addEventListener('click', changeCover);
     coverTitle.innerText = titles[getRandomIndex(titles)];
     tagline1.innerText = descriptors[getRandomIndex(descriptors)];
     tagline2.innerText = descriptors[getRandomIndex(descriptors)];
+  };
+
+  function changeToMakeYourOwn() {
+    homePage.hidden = true;
+    makeYourOwn.hidden = false;
+    saveCoverPage.hidden = true;
   }
 
+  function changeSavePage() {
+    saveCoverPage.hidden = false
+    homePage.hidden = true
+  }
 
-
+  // if (homePage.classList.contains('hidden')) {
+  //   homePage.classList.remove('hidden')
+  // }
+console.log(saveCoverPage)
+console.log(homePage)
 // function changeCover(covers, titles, descriptors) {
 //   for (var i = 0; i < covers.length; i++)
 //     if(currentCover === covers[i]) {
